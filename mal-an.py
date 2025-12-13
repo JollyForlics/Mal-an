a28730d2192d0139cc5afa3862f0f85fe611a82c
Abhinav Adhikari <abhinavadhikari87@gmail.com>
2025-12-13 10:24:10 +0545

    Trying a different framework for getting syscall
    Using `strace` instead of `angr` for syscall for performance reasons

--- mal-an.py
+++ mal-an.py
@@ -3,6 +3,7 @@ from angrutils import plot_cfg
 import cle
 import matplotlib.pyplot as plt
 import networkx as nx
+import subprocess
 import sys
 
 def print_program_graph(project: angr.Project, fileName: str):
@@ -27,14 +28,6 @@ def print_function_call_graph(call_graph: nx.DiGraph, cfg: angr.analyses.CFGEmul
     plt.title("Function Call Graph (CFG Analysis)", size=15)
     plt.show()
 
-syscall_sequence = []
-
-def log_syscalls(state):
-    print("Logging syscall")
-    print(f"State: {state}")
-    syscall_name = state.inspect.syscall_name
-    syscall_sequence.append(syscall_name)
-
 def print_syscall_graph(graph: nx.DiGraph):
     position: dict = nx.spring_layout(graph)
     nx.draw_networkx_nodes(graph, position, node_size=1000, node_color="#3498db")
@@ -43,29 +36,47 @@ def print_syscall_graph(graph: nx.DiGraph):
     plt.title("syscall graph", size=15)
     plt.show()
 
+def get_syscalls(stdout: str):
+    syscalls_of_interest = [
+        'recvfrom', 'write', 'ioctl', 'read', 'sendto', 'dup', 
+        'writev', 'pread', 'close', 'socket', 'bind', 'connect', 
+        'mkdir', 'access', 'chmod', 'open', 'rename', 'fchown32', 
+        'unlink', 'pwrite', 'unmask', 'lseek', 'fcntl', 'recvmsg', 
+        'sendmsg', 'epoll', 'dup2', 'fchown', 'readv', 'chdir'
+    ]
+    syscall_sequence = []
+    
+    line_idx: int = 0
+    events: list[LiteralString] = stdout.splitlines()
+    events.pop(0)
+    events.pop(len(events) - 1)
+    for event in events:
+        char_idx: int = 0
+        new_syscall = ''
+        while event[char_idx] != '(':
+            new_syscall += event[char_idx]
+            char_idx += 1
+        if (new_syscall in syscalls_of_interest):
+            print(new_syscall)
+            syscall_sequence.append(new_syscall)
+        line_idx+=1
+    return(syscall_sequence)
+
 def get_syscall_graph(file: str):
-    project = angr.Project(file, load_options={'auto_load_libs': True})
-    intial_state: angr.SimState = project.factory.entry_state()
-    intial_state.inspect.b('syscall', action=log_syscalls)
-    simulated: angr.SimulationManager = project.factory.simgr(intial_state)
-    simulated.run()
 
-    syscall_graph = nx.DiGraph()
+    strace_command = ['strace', '-f', '-v', file]
+    result = subprocess.run(
+            strace_command,
+            capture_output=True,
+            text=True,
+            check=False
+            )
+    command_stdout = result.stderr.strip()
 
-    for syscall in syscall_sequence:
-        if not syscall_graph.has_node(syscall):
-            syscall_graph.add_node(syscall)
+    syscall_sequence = get_syscalls(command_stdout)
 
-    for i in range(0, len(syscall_sequence) - 1):
-        u = syscall_sequence[i]
-        v = syscall_sequence[i + 1]
 
-        if syscall_graph.has_edge(u, v):
-            syscall_graph[u][v]["weight"] += 1
-        else:
-            syscall_graph.add_edge(u, v, weight=1)
-
-    return(syscall_graph)
+    return nx.DiGraph()
 
 def get_function_call_graph(cfg: angr.analyses.CFGEmulated):
     call_graph: nx.DiGraph = cfg.kb.functions.callgraph
